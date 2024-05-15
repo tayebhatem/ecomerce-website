ca717c19a4edf5ae9f17783df8e3351c68cf9eb0
tayebhatem <tayebhatem98@gmail.com>
2024-05-15 23:19:35 +0200

    new commit

--- actions/checkout.ts
+++ actions/checkout.ts
@@ -7,7 +7,7 @@ export const handlickCheckout=async(total:number)=>{
           Authorization: `Bearer ${process.env.NEXT_PUBLIC_CHARGILY_KEY}`,
           'Content-Type': 'application/json'
         },
-        body: `{"amount":${finalPrice},"currency":"dzd","payment_method":"edahabia","failure_url":"https://dz-comerce.vercel.app/failure","webhook_endpoint":"https://dz-comerce.vercel.app","locale":"ar","collect_shipping_address":false,"success_url":"https://dz-comerce.vercel.app/success"}`
+        body: `{"amount":${finalPrice},"currency":"dzd","payment_method":"edahabia","failure_url":"${process.env.NEXT_PUBLIC_URL}/failure","webhook_endpoint":"${process.env.NEXT_PUBLIC_URL}","locale":"ar","collect_shipping_address":false,"success_url":"${process.env.NEXT_PUBLIC_URL}/success"}`
       };
       
 await fetch('https://pay.chargily.net/test/api/v2/checkouts', options)
